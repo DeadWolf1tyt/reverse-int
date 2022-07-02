85af4564ef4d58f1be3f72beeb38b014e7d944c0
DeadWolf1tyt <deadwolftyt@gmail.com>
2022-07-02 20:41:58 +0300

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+	n = Math.abs(n);
+	let array = [];
+	let str = String(n);
+	for (let i = 0; i < str.length; i++) {
+	array[i] = str[i];
+	}
+	array.reverse();
+	let strRev ;
+	for (let j = 0; j <= array.length; j++) {
+	strRev = strRev + array[j];
+	}
+	let delUndefined = strRev.slice(9, array.length + 9);
+	let numRev = Number(delUndefined);
+	//console.log(numRev);
+	//console.log(typeof numRev);
+	return numRev;
+	//console.log(strRev);
 }
